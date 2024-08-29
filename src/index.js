5841b7bff6916eb805c2f244d0655e60c22d2df3
noob261 <63112861+noob261@users.noreply.github.com>
2024-08-29 11:00:44 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.degenerate.top": dockerHub,
+  "quay.degenerate.top": "https://quay.io",
+  "gcr.degenerate.top": "https://gcr.io",
+  "k8s-gcr.degenerate.top": "https://k8s.gcr.io",
+  "k8s.degenerate.top": "https://registry.k8s.io",
+  "ghcr.degenerate.top": "https://ghcr.io",
+  "cloudsmith.degenerate.top": "https://docker.cloudsmith.io",
+  "ecr.degenerate.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.degenerate.top": dockerHub,
 };
 
 function routeByHosts(host) {
